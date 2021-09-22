ea6269e238d8c0235c86966837d01824a9079dd2
RDA-RW <88998918+RDA-RW@users.noreply.github.com>
2021-09-22 15:04:35 +0700

    Добавьте файлы проекта.

---
+++ RD.SimilarTextsSearcher.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RD.SimilarTextsSearcher.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RD.SimilarTextsSearcher/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RD.SimilarTextsSearcher
+{
+    public class Class1
+    {
+
+        // точка входа
+        // демо приложение
+        // Разные движки поиска SearchEngines (SimMetrics library, FuzzySharp)
+        // Модель входящих данных (разные источники входящих данных)
+        // Модель ответа
+        // Визуализация (объект, json, таблица(библиотека TUI))
+        // Многопоточность
+        // Бенчмарки (Benchmarkdotnet)
+
+
+    }
+}
